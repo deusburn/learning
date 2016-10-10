ed339f83f49ecfd09fb886be1f43afcf6598db17
Serhiy Buta <otaman963@gmail.com>
2016-10-10 18:45:38 +0300

    Added other project for encapsulation tests

---
+++ LearningOOP/OtherProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtherProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //todo: research access to classes A, B, C
+        }
+    }
+}
