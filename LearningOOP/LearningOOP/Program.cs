6bdcca3188d5702f7534b8cea9b3b7887ab5ea36
Serhiy Buta <otaman963@gmail.com>
2016-10-18 15:29:44 +0300

    Task #1

--- LearningOOP/LearningOOP/Program.cs
+++ LearningOOP/LearningOOP/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace LearningOOP
 {
+    //завдання: Створити масив від 1 до 100. Знайти суму всіх чисел, які кратні трьом. (суму вивести на екран)
     class Program
     {
         static void Main(string[] args)
         {
+            int summa = 0;
+            var arrayMen = new int[] {9, 2, 3, 4, 5, 6};//new int[100];
+//            for (int i = 0; i < arrayMen.Length; i++)
+//            {
+//                arrayMen[i] = i + 1;
+//            }
+            for (int i = 0; i < arrayMen.Length; i++)
+            {
+                if ((arrayMen[i] % 3) == 0)
+                {
+                    summa += arrayMen[i];
+                }
+            }
+            Console.Write(summa + " ");
+            Console.ReadKey();
+
         }
     }
 }
