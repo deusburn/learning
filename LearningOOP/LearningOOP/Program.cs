348e9b6d7e0931993afe1b13d05042a59f145cd8
Serhiy Buta <otaman963@gmail.com>
2016-10-18 17:53:50 +0300

    Task #3

--- LearningOOP/LearningOOP/Program.cs
+++ LearningOOP/LearningOOP/Program.cs
@@ -6,29 +6,26 @@ using System.Threading.Tasks;
 
 namespace LearningOOP
 {
-    //Створити масив від 1 до 100. Вивести на екран в зворотньому порядку парні числа більші за 50. (100 98 96 94 92...)
+    //Створити масив від 1 до 20. Вивести числа в рядок, починаючи з середини і до країв.
+    //(приклад для масиву з 6 елементів:
+    //масив: 1 2 3 4 5 6
+    //результат: 3 4 2 5 1 6)
     class Program
     {
         static void Main(string[] args)
         {
-            var array = new int[100];
+            var array = new int[68];
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = i + 1;
             }
-            //for (int i = 0; i < array.Length; i++)
-            //{
-            //    if ((array[i] % 2 == 0) && (array[i] >= 50))
-            //    {
-            //        Console.WriteLine(array[i]);
-            //    }
-            //}
-            for (int i = 99; i >= 0 ; i--)
+            for (int i = 0; i < array.Length / 2 ; i++)
             {
-                if ((array[i] % 2 == 0) && (array[i] >= 50) )
-                {
-                Console.Write(array[i] + " ");
-                }
+                var x2 = array.Length / 2;
+                var x1 = x2 - 1;
+                var pos = x1 - i;
+                var pos1 = i + x2;
+                Console.Write("{0}, {1};   ", array[pos], array[pos1]);
             }
             Console.ReadKey();
         }
