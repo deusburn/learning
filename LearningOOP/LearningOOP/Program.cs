ed98289156a3e19d0db42cba3f76acd026d5af63
Serhiy Buta <otaman963@gmail.com>
2016-10-20 16:36:25 +0300

    Multiplication Table

--- LearningOOP/LearningOOP/Program.cs
+++ LearningOOP/LearningOOP/Program.cs
@@ -6,73 +6,27 @@ using System.Threading.Tasks;
 
 namespace LearningOOP
 {
-    //Створити масив від 1 до 20. Вивести числа в рядок, починаючи з середини і до країв.
-    //(приклад для масиву з 6 елементів:
-    //масив: 1 2 3 4 5 6
-    //результат: 3 4 2 5 1 6)
+    // Таблиця множення
     class Program
     {
+        public static void PrintMultiplicationTable(int number)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                int res = i * number;
+                Console.WriteLine("{0} * {1} = {2} ", number, i, res);
+            }
+        }
         static void Main(string[] args)
         {
-            var array = new int[12];
+            Console.WriteLine("Enter your number");
+            int number = int.Parse(Console.ReadLine());
 
-            InitializeArray(array);
-
-            WritePairsOfElements(array);
+            PrintMultiplicationTable(number);
 
             Console.ReadKey();
         }
 
-        public static void WritePairsOfOddArray(int[] array)
-        {
-            var length = array.Length;
-            var middle = (length - 1) / 2;
-            Console.Write(array[middle] + ";  ");
-
-            var x2 = ((array.Length - 1) / 2) + 1;
-            var x1 = ((array.Length - 1) / 2) - 1;
-
-            for (int i = 0; i < array.Length / 2; i++)
-            {
-                var pos = x1 - i;
-                var pos1 = i + x2;
-
-                Console.Write("{0}, {1};   ", array[pos], array[pos1]);
-            }
-        }
-
-        public static void WritePairsOfEvenArray(int[] array)
-        {
-            var x2 = array.Length / 2;
-            var x1 = x2 - 1;
-
-            for (int i = 0; i < array.Length / 2; i++)
-            {
-                var pos = x1 - i;
-                var pos1 = i + x2;
-
-                Console.Write("{0}, {1};   ", array[pos], array[pos1]);
-            }
-        }
-
-        public static void WritePairsOfElements(int[] array)
-        {
-            if (array.Length % 2 == 0)
-            {
-                WritePairsOfEvenArray(array);
-            }
-            else
-            {
-                WritePairsOfOddArray(array);
-            }
-        }
-
-        public static void InitializeArray(int[] array)
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = i + 1;
-            }
-        }
+        
     }
 }
